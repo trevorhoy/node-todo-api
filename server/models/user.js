4c54193bcaba344949ee129f36a14bf337bc0f94
Trevor Hoy <trevorphoy@gmail.com>
2018-04-05 12:10:43 -0700

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,56 @@
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
+let UserSchema = new mongoose.Schema({
   email: {
     required: true,
     trim: true,
     type: String,
-    minLength: 1
-  }
-})
+    minLength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{Value} is not a vaild email'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minLength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
+});
+
+UserSchema.methods.toJSON = function () {
+  let user = this;
+  let userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+}
+
+UserSchema.methods.generateAuthToken = function () {
+  let user = this;
+  let access = 'auth';
+  let token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
+
+  user.tokens = user.tokens.concat({ access, token });
+
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = { User }
--- server/server.js
+++ server/server.js
@@ -92,6 +92,22 @@ app.patch('/todos/:id', (req, res) => {
   })
 });
 
+app.post('/users', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+  let user = new User({
+    email: body.email,
+    password: body.password
+  });
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user)
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
